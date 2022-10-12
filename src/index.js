22da6c5c2bed060ffcb65578c656ab64bb483052
LIBERCOSOFT <akinspreciousola@gmail.com>
2022-10-12 12:30:37 +0100

    Get all saved scores from api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import axios from 'axios';
 import './styles.css';
 
-const gameOwner = {
-  name: "Kolapo's leaderboard game",
+const scoreContainer = document.querySelector('.scores__container');
+
+const addToScoreContainer = (person) => {
+  const pElement = document.createElement('p');
+  pElement.innerText = `${person.user}: ${person.score}`;
+
+  scoreContainer.appendChild(pElement);
 };
-const response = await axios.post('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/OyocFXKFUQkDPXgPKE3n/scores/', gameOwner);
-console.log(response);
+
+const getScores = async () => {
+  const response = await axios.get('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/OyocFXKFUQkDPXgPKE3n/scores/');
+  const { result } = response.data;
+  result.forEach((val) => addToScoreContainer(val));
+};
+
+getScores();
