d54f7aa2dedbbcea39acd873c66db7ae9058e4f3
LIBERCOSOFT <akinspreciousola@gmail.com>
2022-10-12 12:31:46 +0100

    Send new scores through api

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import './styles.css';
 
 const scoreContainer = document.querySelector('.scores__container');
+const form = document.querySelector('.form');
 
 const addToScoreContainer = (person) => {
   const pElement = document.createElement('p');
@@ -17,3 +18,13 @@ const getScores = async () => {
 };
 
 getScores();
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const user = document.querySelector('#name').value;
+  const score = document.querySelector('#score').value;
+  const newEntry = { user, score };
+  const response = await axios.post('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/OyocFXKFUQkDPXgPKE3n/scores/', newEntry);
+  if (response.status === 201) addToScoreContainer(newEntry);
+});
