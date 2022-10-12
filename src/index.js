574409ab2179191f4411c980b5b878602dc62370
LIBERCOSOFT <akinspreciousola@gmail.com>
2022-10-12 12:29:01 +0100

    Create name of game ID

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+import axios from 'axios';
 import './styles.css';
+
+const gameOwner = {
+  name: "Kolapo's leaderboard game",
+};
+const response = await axios.post('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/OyocFXKFUQkDPXgPKE3n/scores/', gameOwner);
+console.log(response);
