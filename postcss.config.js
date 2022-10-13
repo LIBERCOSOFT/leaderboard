71da44faf7b7ea75637f20d2916ba745a35a02ee
LIBERCOSOFT <akinspreciousola@gmail.com>
2022-10-13 09:25:02 +0100

    Revert "Install tailwind to project"
    This reverts commit 6039e5734f40118c3197bb891ebcef364989a105.

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-const tailwindcss = require('tailwindcss');
-
-module.exports = {
-  plugins: [
-    'postcss-preset-env',
-    tailwindcss,
-  ],
-};
